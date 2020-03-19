25b5eb3467a362f51647ccd32f37645abeb6e852
Taylor Turner <tnt850910@aol.com>
2020-03-19 13:42:38 -0400

    PEP8 Changes

--- Tutorial Codes/Part 1-7/part-7-self-driving-example.py
+++ Tutorial Codes/Part 1-7/part-7-self-driving-example.py
@@ -3,20 +3,21 @@ from PIL import ImageGrab
 import cv2
 import time
 import pyautogui
-from numpy import ones,vstack
+from numpy import ones, vstack
 from numpy.linalg import lstsq
-from directkeys import PressKey,ReleaseKey, W, A, S, D
+from directkeys import PressKey, ReleaseKey, W, A, S, D
 from statistics import mean
 
+
 def roi(img, vertices):
-    
-    #blank mask:
+
+    # blank mask:
     mask = np.zeros_like(img)   
-    
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
+
+    # filling pixels inside the polygon defined by "vertices" with the fill color    
     cv2.fillPoly(mask, vertices, 255)
-    
-    #returning the image only where mask pixels are nonzero
+
+    # returning the image only where mask pixels are nonzero
     masked = cv2.bitwise_and(img, mask)
     return masked
 
@@ -26,26 +27,26 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
     # if this fails, go with some default line
     try:
 
-        # finds the maximum y value for a lane marker 
+        # finds the maximum y value for a lane marker
         # (since we cannot assume the horizon will always be at the same point.)
 
         ys = []  
         for i in lines:
             for ii in i:
-                ys += [ii[1],ii[3]]
+                ys += [ii[1], ii[3]]
         min_y = min(ys)
         max_y = 600
         new_lines = []
         line_dict = {}
 
-        for idx,i in enumerate(lines):
+        for idx, i in enumerate(lines):
             for xyxy in i:
                 # These four lines:
                 # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
                 # Used to calculate the definition of a line, given two sets of coords.
-                x_coords = (xyxy[0],xyxy[2])
-                y_coords = (xyxy[1],xyxy[3])
-                A = vstack([x_coords,ones(len(x_coords))]).T
+                x_coords = (xyxy[0], xyxy[2])
+                y_coords = (xyxy[1], xyxy[3])
+                A = vstack([x_coords, ones(len(x_coords))]).T
                 m, b = lstsq(A, y_coords)[0]
 
                 # Calculating our new, and improved, xs
@@ -62,10 +63,10 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
             m = line_dict[idx][0]
             b = line_dict[idx][1]
             line = line_dict[idx][2]
-            
+
             if len(final_lanes) == 0:
-                final_lanes[m] = [ [m,b,line] ]
-                
+                final_lanes[m] = [[m, b, line]]
+
             else:
                 found_copy = False
 
@@ -74,11 +75,11 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
                     if not found_copy:
                         if abs(other_ms*1.2) > abs(m) > abs(other_ms*0.8):
                             if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
-                                final_lanes[other_ms].append([m,b,line])
+                                final_lanes[other_ms].append([m, b, line])
                                 found_copy = True
                                 break
                         else:
-                            final_lanes[m] = [ [m,b,line] ]
+                            final_lanes[m] = [[m, b, line]]
 
         line_counter = {}
 
@@ -113,24 +114,21 @@ def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
 def process_img(image):
     original_image = image
     # edge detection
-    processed_img =  cv2.Canny(image, threshold1 = 200, threshold2=300)
-    
-    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
-    
-    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],
-                         ], np.int32)
-
+    processed_img = cv2.Canny(image, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
+    vertices = np.array([[10, 500], [10, 300], [300, 200],
+                        [500, 200], [800, 300], [800, 500], ], np.int32)
     processed_img = roi(processed_img, [vertices])
 
     # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-    #                                     rho   theta   thresh  min length, max gap:        
+    #                                     rho   theta   thresh  min length, max gap:       
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,      20,       15)
     m1 = 0
     m2 = 0
     try:
-        l1, l2, m1,m2 = draw_lanes(original_image,lines)
-        cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
-        cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
+        l1, l2, m1, m2 = draw_lanes(original_image, lines)
+        cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0, 255, 0], 30)
+        cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0, 255, 0], 30)
     except Exception as e:
         print(str(e))
         pass
@@ -138,20 +136,23 @@ def process_img(image):
         for coords in lines:
             coords = coords[0]
             try:
-                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
-                
-                
+                cv2.line(processed_img, (coords[0], coords[1]),
+                                        (coords[2], coords[3]), [255, 0, 0], 3)
+
             except Exception as e:
                 print(str(e))
+
     except Exception as e:
         pass
 
-    return processed_img,original_image, m1, m2
+    return processed_img, original_image, m1, m2
+
 
 def straight():
     PressKey(W)
     ReleaseKey(A)
     ReleaseKey(D)
+
 
 def left():
     PressKey(A)
@@ -159,13 +160,16 @@ def left():
     ReleaseKey(D)
     ReleaseKey(A)
 
+
 def right():
     PressKey(D)
     ReleaseKey(A)
     ReleaseKey(W)
     ReleaseKey(D)
 
+
 def slow_ya_roll():
+    PressKey(S)
     ReleaseKey(W)
     ReleaseKey(A)
     ReleaseKey(D)
@@ -176,16 +180,12 @@ for i in list(range(4))[::-1]:
     time.sleep(1)
 
 
-last_time = time.time()
 while True:
-    screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-    print('Frame took {} seconds'.format(time.time()-last_time))
-    last_time = time.time()
-    new_screen,original_image, m1, m2 = process_img(screen)
-    #cv2.imshow('window', new_screen)
-    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
 
-
+    new_screen, original_image = process_img(screen)
+    cv2.imshow('window', new_screen)
+    cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 
     if m1 < 0 and m2 < 0:
         right()
@@ -193,9 +193,7 @@ while True:
         left()
     else:
         straight()
-    
-    #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
-
